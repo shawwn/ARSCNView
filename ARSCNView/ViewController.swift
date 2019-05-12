4d42900c6c84231aa996b84f4f51b5519911e692
Shawn Presser <shawnpresser@gmail.com>
2019-05-12 08:31:57 -0500

    Initial Commit

---
+++ ARSCNView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ARSCNView
+//
+//  Created by Emily Kolar on 5/12/19.
+//  Copyright © 2019 sweetiebird. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+

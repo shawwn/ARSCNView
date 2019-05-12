62bf633948302fac9c63e1a7bc43a1ac804653d4
Shawn Presser <shawnpresser@gmail.com>
2019-05-12 08:40:34 -0500

    Initial box

--- ARSCNView/ViewController.swift
+++ ARSCNView/ViewController.swift
@@ -7,14 +7,38 @@
 //
 
 import UIKit
+import ARKit
 
 class ViewController: UIViewController {
 
+  @IBOutlet weak var sceneView: ARSCNView!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+    addBox()
   }
 
-
+  override func viewWillAppear(_ animated: Bool) {
+      super.viewWillAppear(animated)
+      let configuration = ARWorldTrackingConfiguration()
+      sceneView.session.run(configuration)
+  }
+  
+  override func viewWillDisappear(_ animated: Bool) {
+      super.viewWillDisappear(animated)
+      sceneView.session.pause()
+  }
+  
+  func addBox() {
+    let box = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0)
+    
+    let boxNode = SCNNode()
+    boxNode.geometry = box
+    boxNode.position = SCNVector3(0, 0, -0.2)
+    
+    let scene = SCNScene()
+    scene.rootNode.addChildNode(boxNode)
+    sceneView.scene = scene
+  }
 }
 

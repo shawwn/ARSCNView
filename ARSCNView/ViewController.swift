b1d7409e7014b38fe2ae049c685a16b06f835d28
Shawn Presser <shawnpresser@gmail.com>
2019-05-12 09:28:44 -0500

    Drawing

--- ARSCNView/ViewController.swift
+++ ARSCNView/ViewController.swift
@@ -15,6 +15,8 @@ class ViewController: UIViewController {
   
   override func viewDidLoad() {
     super.viewDidLoad()
+    sceneView.debugOptions = [.showWireframe, .showWorldOrigin, .showBoundingBoxes, .showSkeletons, .showFeaturePoints, .showLightInfluences]
+    addTapGestureToSceneView()
     addBox()
   }
 
@@ -29,16 +31,77 @@ class ViewController: UIViewController {
       sceneView.session.pause()
   }
   
+  func addTapGestureToSceneView() {
+      let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(ViewController.didTap(withGestureRecognizer:)))
+      sceneView.addGestureRecognizer(tapGestureRecognizer)
+      let panGestureRecognizer = UIPanGestureRecognizer(target: self, action: #selector(ViewController.didPan(withGestureRecognizer:)))
+      sceneView.addGestureRecognizer(panGestureRecognizer)
+  }
+  
+  @objc func didTap(withGestureRecognizer recognizer: UIGestureRecognizer) {
+      let tapLocation = recognizer.location(in: sceneView)
+      let hitTestResults = sceneView.hitTest(tapLocation)
+      let featureTestResults = sceneView.hitTest(tapLocation, types: ARHitTestResult.ResultType.featurePoint)
+      guard let node = hitTestResults.first?.node else { 
+        if let result = featureTestResults.first {
+          addBox(result.worldTransform)
+        }
+        return
+      }
+      node.removeFromParentNode()
+  }
+  
+  @objc func didPan(withGestureRecognizer recognizer: UIGestureRecognizer) {
+      let tapLocation = recognizer.location(in: sceneView)
+//      let hitTestResults = sceneView.hitTest(tapLocation)
+      let featureTestResults = sceneView.hitTest(tapLocation, types: ARHitTestResult.ResultType.featurePoint)
+//      guard let node = hitTestResults.first?.node else { 
+        if var result = featureTestResults.first {
+          for feature in featureTestResults {
+            if feature.distance > result.distance {
+              result = feature
+            }
+          }
+          addBox(result.worldTransform)
+        }
+//        return
+//      }
+//      node.removeFromParentNode()
+  }
+  
   func addBox() {
-    let box = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0)
+    let box = SCNBox(width: 0.001, height: 0.001, length: 0.001, chamferRadius: 0)
     
     let boxNode = SCNNode()
     boxNode.geometry = box
+    boxNode.opacity = 0.1
     boxNode.position = SCNVector3(0, 0, -0.2)
     
     let scene = SCNScene()
     scene.rootNode.addChildNode(boxNode)
     sceneView.scene = scene
+  }
+  
+  func addBox(_ transform: simd_float4x4) {
+    let box = SCNBox(width: 0.01, height: 0.01, length: 0.01, chamferRadius: 0)
+    
+    let boxNode = SCNNode()
+    boxNode.geometry = box
+    boxNode.opacity = 0.1
+    boxNode.position = SCNVector3(0, 0, 0)
+    boxNode.simdTransform = transform
+    
+    sceneView.scene.rootNode.addChildNode(boxNode)
+  }
+  
+  func addBox2() {
+    let box = SCNBox(width: 0.05, height: 0.05, length: 0.05, chamferRadius: 0)
+    
+    let boxNode = SCNNode()
+    boxNode.geometry = box
+    boxNode.position = SCNVector3(0, 0, -0.2)
+    
+    sceneView.scene.rootNode.addChildNode(boxNode)
   }
 }
 
